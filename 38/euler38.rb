b9282611b67799247a7fca7e44ba3c9dfdaa3165
YoniA <YoniA@cat.com>
2022-01-04 19:30:25 +0200

    add 38 solution

--- 38/euler38.rb
+++ 38/euler38.rb
@@ -1,17 +1,34 @@
 max = 123456789
 
 def is_pandigital(n)
-	n.digits.uniq.size == 9
+	n.digits.uniq.sort.join == "123456789"
 end
 
 
-n
-(1..9).each do |d|
-	result += (result.to_i * d).to_s
-	
-	if (is_pandigital(result.to_i) && (result.to_i > max))
-		max = result
+def has_unique_digits(n)
+	n.digits.uniq.size == n.digits.size
+end
+
+(1..10000).each do |n|
+	curr = n
+	(2..9).each do |i|
+		curr = curr.to_s + (i*n).to_s
+		unless has_unique_digits(curr.to_i)
+			break
+		end
+		
+		unless curr.to_i.digits.size < 10
+			break
+		end
+		
+		if (is_pandigital(curr.to_i) && (curr.to_i > max))
+			max = curr.to_i
+			puts "new max is: #{max}"
+		end	
 	end
 end
+
+
+puts max
 
 
