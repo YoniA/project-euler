fc8ac4b45a571479a8ed7736305920bb026c1e28
YoniA <YoniA@cat.com>
2022-01-25 20:34:20 +0200

    add 50 solution

--- 50/euler50.rb
+++ 50/euler50.rb
@@ -10,9 +10,19 @@ def is_prime(n)
         return true
 end
 
-primes = (2..999).filter { |n| is_prime(n) }
+primes = (2..999999).filter { |n| is_prime(n) }
 
+MAX_WIN_SIZE = primes.size
+size = 2
 
-max = 6
-
-
+while size < MAX_WIN_SIZE
+	(0...(MAX_WIN_SIZE - size)).each do |i|
+		s = primes[i..(i+size)].sum
+		if primes.bsearch { |p|  s <=> p }
+			size += 1
+			puts "size: #{size}, p: #{s}"
+			break	
+		end
+	end
+	size += 1
+end
